241202b9d409b86f43b09524e15c69ebbbc71f80
Dhiraj Chhawchharia <dhirajchhawchharia27@gmail.com>
2024-12-10 20:11:33 +0530

    test improved

--- hello_server_test.go
+++ hello_server_test.go
@@ -16,12 +16,12 @@ func TestGreetingSpecificDemo(t *testing.T) {
 	}
 }
 
-func TestShowFailure(t *testing.T) {
-	greeting := CreateGreeting("Demo1")
-	if greeting != "Hello, Demo\n" {
-		t.Errorf("Intentional failure. got: %s, want: %s.", greeting, "Hello, Demo\n")
-	}
-}
+// func TestShowFailure(t *testing.T) {
+// 	greeting := CreateGreeting("Demo")
+// 	if greeting != "Hello, Demo\n" {
+// 		t.Errorf("Intentional failure. got: %s, want: %s.", greeting, "Hello, Demo\n")
+// 	}
+// }
 
 
 
